764dfb2a94dbd9e6b5b423af4585f2a3a279c8fe
lawrhence015 <lawrhence015@gmail.com>
2024-01-16 23:51:42 +0000

    printing a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to the strin
+ *
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}	
+	if (*s == '\0')
+	{
+	_putchar ('\n');
+	}
+}
